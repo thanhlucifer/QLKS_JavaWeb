eaa4a17801f3b0cf28d749d68c1175428980b48f
KevinHuynh <108676435+KevinHuynh101@users.noreply.github.com>
2023-06-17 16:42:08 +0700

    update food uesr

--- src/main/java/qlks/java/qlks_javaweb/repository/FoodRepository.java
+++
@@ -1,9 +0,0 @@
-package qlks.java.qlks_javaweb.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import qlks.java.qlks_javaweb.model.food_item;
-
-@Repository
-public interface FoodRepository extends JpaRepository<food_item,Long> {
-}
